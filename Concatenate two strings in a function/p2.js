bf1540494b82310766b79c2f9aa06c9ddae04d5f
Utka2000 <utkarsh.yashraj@dymensions.io>
2023-05-31 16:58:38 +0530

    adding folder

---
+++ Concatenate two strings in a function/p2.js
@@ -0,0 +1,11 @@
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+function myfun()
+{
+    console.log(greeting +", "+name+"!")
+    
+}
+
+myfun()
